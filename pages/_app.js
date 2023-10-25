3226561802745e59b46583f086e01e7bffa943fc
khairul-amin-ds <khairul.amin@digitalsymphony.it>
2023-10-25 10:46:37 +0800

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import '@/styles/custom.css'
-import 'slick-carousel/slick/slick.css'; 
-import 'slick-carousel/slick/slick-theme.css';
+import Head from 'next/head';
 import Navmenu from '../src/components/Navmenu';
 import Footer from '@/src/components/Footer';
 export default function App({ Component, pageProps }) {
   return (
     <>
+        <Head>
+        </Head>
         <Navmenu></Navmenu>
         <Component {...pageProps} />
         <Footer></Footer>
