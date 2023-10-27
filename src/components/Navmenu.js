e9f3260736085f0ee857aa30dc2a7836fac3b3ef
Aisyah Ishak <82633035+AisyahIshak@users.noreply.github.com>
2023-10-27 20:44:59 +0800

    add navbar on scroll behavior

--- src/components/Navmenu.js
+++ src/components/Navmenu.js
@@ -1,10 +1,33 @@
 import { Navbar } from "flowbite-react";
 import Link from "next/link";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 const Navmenu = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 0) {
+          setIsScrolled(true);
+        } else {
+          setIsScrolled(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    const navbarClass = `hidden md:block md:p-0 fixed h-[100px] w-full border-gray-200 z-[40] shadow-sm transition ease-in-out delay-150 ${
+      isScrolled ? 'bg-black bg-opacity-100 ' : 'bg-[#0000006e] to-opacity-50'
+    }`;
+
     return(
         <>
-        <nav className="hidden md:block md:p-0 fixed h-[100px] w-full bg-[#0000006e] to-opacity-50 border-gray-200 z-[40] shadow-sm">
+        <nav className={navbarClass}>
             <div className="flex justify-center items-center h-full">
                 <Link className="font-GothamBook uppercase tracking-wider text-[20px] md:text-[14px] 3xl:text-[16px] mr-7" href="/">Concept</Link>
                 <Link className="font-GothamBook uppercase tracking-wider text-[20px] md:text-[14px] 3xl:text-[16px] mr-7" href="/features">Features</Link>
