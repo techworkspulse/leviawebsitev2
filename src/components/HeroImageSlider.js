24cafe4aa0e81525ac1bc5a5f2146f57ebf54275
khairul-amin-ds <khairul.amin@digitalsymphony.it>
2023-11-07 18:15:10 +0800

    updates arrow

--- src/components/HeroImageSlider.js
+++ src/components/HeroImageSlider.js
@@ -4,18 +4,19 @@ import Image from 'next/image';
 
 const HeroImageSlider = ({img1, img2, img3}) => {
     const settings = {
-      arrows:false,
+      arrows:true,
       dots: false,
       infinite: true,
-      autoplay:true,
+      autoplay:false,
       adaptiveHeight:true,
+      accessibility: true,
       speed: 500,
       margin:0,
       slidesToShow: 1,
       slidesToScroll: 1
     };
     return (
-      <Slider {...settings}>
+      <Slider {...settings} className="heroimageslider">
             <div className="relative">
               <Image src={`${img1}`} className="w-full h-[500px] lg:h-[700px] object-cover" alt="Facebook" width="1920" height="1000"/>
             </div>
